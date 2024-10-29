4d95c5f3a017c365882c3ba74543d42aab5d8e78
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-29 21:29:44 +0900

    :sparkles: add cbmc-viewer and kani-verifier

--- flake.nix
+++ flake.nix
@@ -31,7 +31,63 @@
         formatter = pkgs.nixfmt-rfc-style;
 
         packages = rec {
+          cbmc-viewer = pkgs.python3Packages.buildPythonApplication rec {
+            pname = "cbmc-viewer";
+            version = "3.10";
+            format = "pyproject";
 
+            src = pkgs.fetchFromGitHub {
+              owner = "model-checking";
+              repo = pname;
+              rev = "viewer-${version}";
+              hash = "sha256-+s5RBC3XSgb8omTbUNLywZnP6jSxZBKSS1BmXOjRF8M=";
+              fetchSubmodules = true;
+            };
+
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              setuptools
+              jinja2
+              voluptuous
+            ];
+
+            # for dev TODO:
+            buildPhase = ''
+              echo "Contents of source directory:"
+              ls -la
+              cat README.md
+              # Now call the default pypa build phase
+              pypaBuildPhase
+            '';
+          };
+
+          kani-verifier = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "kani-verifier";
+            version = "0.56.0";
+
+            buildInputs = with pkgs; [ kissat cbmc ] ++ [ cbmc-viewer ];
+
+            src = pkgs.fetchFromGitHub {
+              owner = "model-checking";
+              repo = "kani";
+              rev = "kani-${version}";
+              hash = "sha256-+s5RBC3XSgb8omTbUNLywZnP6jSxZBKSS1BmXOjRF8M=";
+              fetchSubmodules = true;
+            };
+
+            cargoHash = pkgs.lib.fakeHash;
+
+            # ref: https://github.com/model-checking/kani/blob/52fcc6cc747779c31ba41593b0d0777043540264/.github/actions/build-bundle/action.yml
+            buildPhase = ''
+              cargo bundle -- ${version}
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp target/release/* $out/bin/
+            '';
+          };
+
+          default = kani-verifier;
         };
       };
     };
