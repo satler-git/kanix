fdb63b17401b3bf63bb977465c8ddae520329473
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-29 19:57:05 +0900

    :sparkles: add very basic flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,37 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "nixpkgs";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { config, system, pkgs, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
+        };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        formatter = pkgs.nixfmt-rfc-style;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        packages = rec {
 
-  };
+        };
+      };
+    };
 }
